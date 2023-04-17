696c41a05526dfa8ecab7b799b1e8137f8bdc9ea
ukrwebprom <ukrwebprom@gmail.com>
2023-04-17 17:45:36 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { UserProvider } from 'firebase-func';
-import { BrowserRouter } from 'react-router-dom';
+/* import { BrowserRouter } from 'react-router-dom'; */
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -14,11 +15,11 @@ import { lightTheme } from 'Theme';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
-      <BrowserRouter basename='/websocket-chat'>
+      <HashRouter basename='/websocket-chat'>
       <ThemeProvider theme={lightTheme}>
         <App />
       </ThemeProvider>
-      </BrowserRouter>
+      </HashRouter>
     </UserProvider>
   </React.StrictMode>
 );
