195e1f94e5e637bd9ca17bc678703d1a8d2bb0f3
ukrwebprom <ukrwebprom@gmail.com>
2023-04-18 21:34:34 +0300

    fgfgndnfgn

--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -1,12 +1,12 @@
 
-import { ChatModule } from "components/ChatModule/ChatModule";
+/* import { ChatModule } from "components/ChatModule/ChatModule"; */
 import { Container } from '@mui/material';
 
 export const Chat = () => {
 
     return(
         <Container maxWidth="xl">
-            <ChatModule />    
+{/*             <ChatModule />   */}  
         </Container>
         
     )
