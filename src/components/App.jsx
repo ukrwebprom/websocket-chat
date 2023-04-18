d5903cd516de358ee97524422ffc147b90744981
ukrwebprom <ukrwebprom@gmail.com>
2023-04-18 20:27:21 +0300

    bdfbdfb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "routes/Layout";
 import { Mainpage } from "routes/Mainpage";
 import { Chat } from "routes/Chat";
-import { useUser } from 'firebase-func';
+/* import { useUser } from 'firebase-func'; */
 import './app.scss';
 
 
 
 export const App = () => {
-  const { user } = useUser();
+/*   const { user } = useUser(); */
   return (
       <Routes>
         <Route path="/" element={<Layout />}>
