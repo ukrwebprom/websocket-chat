ef567cb9356dae0dcfdef501dc18800eba546505
ukrwebprom <ukrwebprom@gmail.com>
2023-04-18 20:24:15 +0300

    dfbdfbdfb

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,8 +13,9 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Mainpage />} />
-          {user? <Route path=":chatID" element={<Chat />} /> :
-          <Route path=":chatID" element={<Mainpage />} />}
+          <Route path=":chatID" element={<Chat />} />
+{/*           {user? <Route path=":chatID" element={<Chat />} /> :
+          <Route path=":chatID" element={<Mainpage />} />} */}
         </Route>
         
       </Routes>
