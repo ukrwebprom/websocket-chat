cc7d825795a5c255aa8bf5ebbca82702a9c56efa
ukrwebprom <ukrwebprom@gmail.com>
2023-04-20 00:54:14 +0300

    sender

--- src/components/ChatModule/ChatModule.jsx
+++ src/components/ChatModule/ChatModule.jsx
@@ -105,7 +105,7 @@ export const ChatModule = ({ ID }) => {
       paper.current.scrollTo(0, paper.current.scrollHeight);
       console.log(paper.current);
     }
-  }, [lastMessage]);
+  }, [lastMessage, sender]);
 
 
 
