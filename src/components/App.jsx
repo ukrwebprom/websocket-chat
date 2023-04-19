3085f239b0361fb572cc759211196b6ca81d5da4
ukrwebprom <ukrwebprom@gmail.com>
2023-04-19 18:41:01 +0300

    dsasdadg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,22 @@ import { Routes, Route } from "react-router-dom";
 import { Layout } from "routes/Layout";
 import { Mainpage } from "routes/Mainpage";
 import { Chat } from "routes/Chat";
-/* import { useUser } from 'firebase-func'; */
+import { useUser } from 'firebase-func';
 import './app.scss';
 
 
 
 export const App = () => {
-/*   const { user } = useUser(); */
+  const { user } = useUser();
   return (
       <Routes>
         <Route path="/" element={<Layout />} >
-          <Route index element={<Mainpage />} />
-          <Route path=":chatID" element={<Chat />} />
+          {/* <Route index element={<Mainpage />} />
+          <Route path=":chatID" element={<Chat />} /> */}
+          {user? <Route path=":chatID" element={<Chat />} /> :
+          <Route path=":chatID" element={<Mainpage />} />}
         </Route>
-{/*           {user? <Route path=":chatID" element={<Chat />} /> :
-          <Route path="/chat/:chatID" element={<Mainpage />} />} */}
+
         
       </Routes>
   );
